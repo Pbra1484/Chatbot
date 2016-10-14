77e189021a1da375d40f56579a3f6c619afb751e
Pbra1484 <pbra1484@740s-pl-108.local>
2016-10-14 12:47:37 -0600

    Added tests and JUnit

---
+++ src/chat/controller/ChatbotRunner.java
@@ -0,0 +1,12 @@
+package chat.controller;
+
+public class ChatbotRunner
+{
+	public static void main(String [] args)
+	{
+		ChatbotController chatbotAppController = new ChatbotController();
+		chatbotAppController.start();
+	}
+	
+	
+}
--- src/chat/view/ChatViewer.java
+++ src/chat/view/ChatViewer.java
@@ -1,4 +1,4 @@
-package view;
+package chat.view;
 
 public class ChatViewer
 {
