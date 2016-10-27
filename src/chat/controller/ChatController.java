0dedc26cbd4c7cf913f02358690c98b34df0c890
Pbra1484 <pbra1484@740s-pl-108.local>
2016-10-27 12:53:36 -0600

    added use cabot checkers

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -18,15 +18,35 @@ public class ChatController
 	
 	public void start()
 	{
-		String response = "talking";
+		String response = "Hello";
 		
 		while(stupidBot.lengthChecker(response))
 		{
+			chatView.displayMessage(useChatbotCheckers(response));
 			response = chatView.collectResponse("What to talk about today?");
 			
 		}
 		
+	}
+	
+	private String useChatbotCheckers(String input)
+	{
+		String answer = "";
 		
+		if(stupidBot.contentChecker(input))
+		{
+			answer += "\nSpecal Secret\n";
+		}
+		if(stupidBot.memeChecker(input))
+		{
+			answer += "\nMemes\n";
+		}
+		if(answer.length() == 0)
+		{
+			answer += "Sorry, I don't understand " + input;
+		}
+		
+		return answer;
 	}
 
 }
