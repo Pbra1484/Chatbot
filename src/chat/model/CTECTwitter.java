912f7a41ec68a62104dc8c58c66750dcf0abb42f
Pbra1484 <pbra1484@740s-pl-123.local>
2017-03-06 13:53:56 -0700

    made word lists

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -4,16 +4,24 @@ import chat.controller.ChatController;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.TwitterException;
+import twitter4j.Status;
+import java.util.List;
+import java.util.ArrayList;
 
 public class CTECTwitter 
 {
 	private ChatController baseController;
 	private Twitter twitterBot;
+	private List<Status> searchedTweets;
+	private List<String> ignoredWords;
+	
 	
 	public CTECTwitter(ChatController baseController)
 	{
 		this.baseController = baseController;
 		twitterBot = TwitterFactory.getSingleton();
+		ignoredWords = new ArrayList<String>();
+		searchedTweets = new ArrayList<Status>();
 	}
 	
 	public void sendTweet(String textToTweet)
@@ -30,5 +38,15 @@ public class CTECTwitter
 		{
 			baseController.handleErrors(otherError);
 		}
+	}
+	
+	private void createIgnoredWordList()
+	{
+		
+	}
+	
+	private String getMostCommonWord()
+	{
+		return null;
 	}
 }
