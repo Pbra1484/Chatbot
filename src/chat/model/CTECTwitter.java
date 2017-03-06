ac80f13f96964993c0124c2a56127ac99617687f
Pbra1484 <pbra1484@740s-pl-123.local>
2017-03-06 12:06:39 -0700

    added twitter4j

---
+++ src/chat/model/CTECTwitter.java
@@ -0,0 +1,13 @@
+package chat.model;
+
+import chat.controller.ChatController;
+
+public class CTECTwitter 
+{
+	private ChatController baseController;
+	
+	public CTECTwitter(ChatController baseController)
+	{
+		this.baseController = baseController;
+	}
+}
